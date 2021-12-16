b1389f6e0c55b636a383b01605f3f7670d28a404
Olaleye Blessing <Olayinkablexxy@gmail.com>
2021-12-16 20:00:50 +0100

    add resume link

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -4,7 +4,11 @@ import { StyledResume } from "./Style/StyledResume";
 
 const Resume: FC = () => {
     return (
-        <StyledResume>
+        <StyledResume
+            href="https://resume.io/r/MtzvmHqJI"
+            target="_blank"
+            rel="noreferrer noopener"
+        >
             <span>check my resume</span>
             <figure aria-hidden="true">
                 <AiOutlineFilePdf />
