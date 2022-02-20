70a0c2f31c7fc105636fa2b9793d12fb48e97b50
Olaleye Blessing <Olayinkablexxy@gmail.com>
2022-02-20 10:39:21 +0100

    update meta tags

--- components/HomeMetaHead.tsx
+++ components/HomeMetaHead.tsx
@@ -5,11 +5,27 @@ const HomeMetaHead: FC = () => {
     return (
         <Head>
             <title>Olaleye Blessing | Frontend Developer</title>
+
+            {/* Theme Color for Chrome, Firefox OS and Opera */}
+            <meta name="theme-color" content="#ff0a78" />
+
+            {/* Control the behavior of search engine crawling and indexing */}
+            <meta name="robots" content="index,follow" />
+
+            {/* Short description of your document's subject */}
+            <meta name="subject" content="Olaleye Blessing's portfolio" />
+
+            {/* Gives a general age rating based on the document's content */}
+            <meta name="rating" content="General" />
+
             <meta name="Author" content="Olaleye Blessing" />
             <meta
                 name="description"
                 content="A passionate frontend developer focused on creating interactive, accessible and responsive websites"
             />
+
+            {/* twitter card */}
+            <meta name="twitter:card" content="summary" />
             <meta name="twitter:creator" content="@OlaleyeBlessin" />
             <meta
                 name="twitter:title"
@@ -19,6 +35,9 @@ const HomeMetaHead: FC = () => {
                 name="twitter:description"
                 content="A passionate frontend developer focused on creating interactive, accessible and responsive websites"
             />
+            {/*  */}
+
+            {/* Facebook Open Graph */}
             <meta
                 property="og:description"
                 content="A passionate frontend developer focused on creating interactive, accessible and responsive websites"
