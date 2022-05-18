9d17747cf4e09ff90fa019bb881beed52a6ddd19
Olaleye Blessing <Olayinkablexxy@gmail.com>
2022-05-18 16:15:03 +0100

    update: fix twitter url

--- utils/socails.ts
+++ utils/socails.ts
@@ -3,18 +3,18 @@ import { FaLinkedinIn } from "react-icons/fa";
 
 export let socials = [
     {
-        href: "https://twitter.com/OlaleyeBlessin",
+        href: "https://twitter.com/_jongbo",
         Icon: AiOutlineTwitter,
         label: "twitter",
     },
     {
         href: "https://github.com/Olaleye-Blessing",
         Icon: AiFillGithub,
-        label: "twitter",
+        label: "github",
     },
     {
         href: "https://www.linkedin.com/in/blessing-olaleye-139a22204/",
         Icon: FaLinkedinIn,
-        label: "twitter",
+        label: "linkedin",
     },
 ];
