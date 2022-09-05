d59f5d36f5287de563d81e0ca1d0843d68bc31c7
Olaleye Blessing <Olayinkablexxy@gmail.com>
2022-09-06 00:34:31 +0100

    Update resume

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,40 +5,38 @@ import { StyledHeader } from "./Style/StyledHomeContent";
 import headerBg from "./../public/static/images/header.svg";
 
 const Header: FC = () => {
-    return (
-        <StyledHeader className="">
-            <div className="container">
-                <div className="header__content">
-                    <h1>
-                        <span className="header__greeting">
-                            Hello Friend! I&apos;m{" "}
-                        </span>
-                        <span className="header__name">Olaleye Blessing,</span>
-                    </h1>
-                    <p className="header__intro">
-                        a passionate frontend developer focused on creating
-                        interactive, accessible, and responsive websites.
-                    </p>
-                    <div className="header__links">
-                        <Link href="/#projects">
-                            <a className="btn__primary btn">See My Work</a>
-                        </Link>
-                        <a
-                            className="btn__secondary btn"
-                            href="https://resume.io/r/MtzvmHqJI"
-                            target="_blank"
-                            rel="noreferrer noopener"
-                        >
-                            View Resume
-                        </a>
-                    </div>
-                </div>
-                <div className="header__svg" aria-hidden="true">
-                    <Image alt="" src={headerBg} layout="responsive" />
-                </div>
-            </div>
-        </StyledHeader>
-    );
+  return (
+    <StyledHeader className="">
+      <div className="container">
+        <div className="header__content">
+          <h1>
+            <span className="header__greeting">Hello Friend! I&apos;m </span>
+            <span className="header__name">Olaleye Blessing,</span>
+          </h1>
+          <p className="header__intro">
+            a passionate frontend developer focused on creating interactive,
+            accessible, and responsive websites.
+          </p>
+          <div className="header__links">
+            <Link href="/#projects">
+              <a className="btn__primary btn">See My Work</a>
+            </Link>
+            <a
+              className="btn__secondary btn"
+              href="https://docs.google.com/document/d/1u8sv0wNbkGsq2eGa3yHVyOvfn5x294Xsn10QhtdxAOg/edit"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              View Resume
+            </a>
+          </div>
+        </div>
+        <div className="header__svg" aria-hidden="true">
+          <Image alt="" src={headerBg} layout="responsive" />
+        </div>
+      </div>
+    </StyledHeader>
+  );
 };
 
 export default Header;
--- components/Resume.tsx
+++ components/Resume.tsx
@@ -3,18 +3,18 @@ import { AiOutlineFilePdf } from "react-icons/ai";
 import { StyledResume } from "./Style/StyledResume";
 
 const Resume: FC = () => {
-    return (
-        <StyledResume
-            href="https://resume.io/r/MtzvmHqJI"
-            target="_blank"
-            rel="noreferrer noopener"
-        >
-            <span>check my resume</span>
-            <figure aria-hidden="true">
-                <AiOutlineFilePdf />
-            </figure>
-        </StyledResume>
-    );
+  return (
+    <StyledResume
+      href="https://docs.google.com/document/d/1u8sv0wNbkGsq2eGa3yHVyOvfn5x294Xsn10QhtdxAOg/edit"
+      target="_blank"
+      rel="noreferrer noopener"
+    >
+      <span>check my resume</span>
+      <figure aria-hidden="true">
+        <AiOutlineFilePdf />
+      </figure>
+    </StyledResume>
+  );
 };
 
 export default Resume;
