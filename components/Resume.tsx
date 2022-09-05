68de038d8bf15102cff4a7253bcbb376f9fc7c2d
Olaleye Blessing <Olayinkablexxy@gmail.com>
2022-09-06 00:34:31 +0100

    Update resume

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -3,18 +3,14 @@ import { AiOutlineFilePdf } from "react-icons/ai";
 import { StyledResume } from "./Style/StyledResume";
 
 const Resume: FC = () => {
-    return (
-        <StyledResume
-            href="https://resume.io/r/MtzvmHqJI"
-            target="_blank"
-            rel="noreferrer noopener"
-        >
-            <span>check my resume</span>
-            <figure aria-hidden="true">
-                <AiOutlineFilePdf />
-            </figure>
-        </StyledResume>
-    );
+  return (
+    <StyledResume href="/resume.pdf" target="_blank" rel="noreferrer noopener">
+      <span>check my resume</span>
+      <figure aria-hidden="true">
+        <AiOutlineFilePdf />
+      </figure>
+    </StyledResume>
+  );
 };
 
 export default Resume;
