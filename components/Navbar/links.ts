6a913e08dea72d1165fbfcde7835298bfbbc0bf2
Olaleye Blessing <Olayinkablexxy@gmail.com>
2023-02-25 14:53:14 +0100

    Go to correct section when navbar link is clicked

--- components/Navbar/links.ts
+++ components/Navbar/links.ts
@@ -1,7 +1,7 @@
 export const Links = [
   { path: "/", text: "HOME" },
-  { path: "#aboutme", text: "ABOUT ME" },
-  { path: "#projects", text: "PROJECTS" },
-  { path: "#contact", text: "CONTACT" },
+  { path: "/#aboutme", text: "ABOUT ME" },
+  { path: "/#projects", text: "PROJECTS" },
+  { path: "/#contact", text: "CONTACT" },
   { path: "/os", text: "OS" },
 ];
