705948d17a6687f8034ea70ef6d508a55523c84e
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-08-17 09:05:30 +0100

    Update bio

--- components/home/header.tsx
+++ components/home/header.tsx
@@ -1,15 +1,17 @@
 export default function Header() {
 	return (
-		<header className='h-screen flex flex-col items-center justify-center text-center px-4'>
+		<header className='h-[90vh] flex flex-col items-center justify-center text-center px-4 sm:h-[93vh]'>
 			<h1 className='text-5xl mt-auto text-muted-foreground shadow-inner'>
 				Olaleye Blessing
 			</h1>
 			<p>
-				I&apos;m a software developer dedicated to creating{' '}
-				<span className='text-primary font-bold'>interactive</span>,{' '}
-				<span className='text-primary font-bold'>accessible</span>, and{' '}
-				<span className='text-primary font-bold'>responsive</span>{' '}
-				websites.
+				I&apos;m a full-stack developer with a strong foundation in
+				frontend development. With over{' '}
+				<em className='font-bold text-muted-foreground'>
+					3 years of experience
+				</em>{' '}
+				crafting engaging user interfaces, I&apos;ve developed my skills
+				in building scalable web applications.
 			</p>
 			<p className='mt-auto mb-8'>Scroll down</p>
 		</header>
